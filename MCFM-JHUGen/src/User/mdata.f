8d1530ffb8ec08d048b33c03d62f7966cf66029b
Nicholas <npinto7@jh.edu>
2026-09-08 22:58:15 +0200

    Add cW to user-defined couplings.

--- MCFM-JHUGen/src/User/mdata.f
+++ MCFM-JHUGen/src/User/mdata.f
@@ -379,6 +379,9 @@ c     anomalous couplings for triple and quartic gauge boson coupling with d_4 (
       data dFour_A / (0d0,0d0) /
       data dFour_Z / (0d0,0d0) /
 
+c     anomalous couplings for non-higgs-containing triple / quartic gauge boson interactions. (SM: set all to 0)
+      data cW / (0d0,0d0) / 
+      
 c     right handed Z couplings to charged leptons (set separately for each Z)
       data reZ / 0d0 / ! SM = (-2*-1*xw_inp)/(2*sqrt(xw_inp*(1-xw_inp)))
 
